4b3a4fc89cd8787fe80c169112fdf9d0859ee756
moee <moee@github>
2012-10-26 14:20:03 +0200

    Removed jUnit target from custom_rules.xml

--- detlef/tests/at/ac/tuwien/detlef/MainActivityTest.java
+++ detlef/tests/at/ac/tuwien/detlef/MainActivityTest.java
@@ -3,29 +3,29 @@ package at.ac.tuwien.detlef;
 import com.jayway.android.robotium.solo.Solo;
 
 import android.test.ActivityInstrumentationTestCase2;
-import android.test.InstrumentationTestRunner;
 
 public class MainActivityTest extends
 	ActivityInstrumentationTestCase2<MainActivity> {
 
 	private Solo solo;
 	
-	private MainActivity ma = new MainActivity();
-	
-	private static Class<MainActivity> cma = MainActivity.class;
-
 	public void setUp() throws Exception {
-		InstrumentationTestRunner ru = new android.test.InstrumentationTestRunner();
 		solo = new Solo(getInstrumentation(), getActivity());
 	}
 
 	public MainActivityTest() {
-		super(cma);
+		super(MainActivity.class);
 	}
 
-	public void test() {
-		assertTrue(true);
+	/**
+	 * Makes sure that a menu item called "All Podcasts" appears
+	 * on the first page.
+	 * @throws Exception
+	 */
+	public void testAllPodcatsOnFirstPage() throws Exception {
+        assertTrue(solo.searchText("All Podcasts"));
 	}
+
 	
    @Override
    public void tearDown() throws Exception {
