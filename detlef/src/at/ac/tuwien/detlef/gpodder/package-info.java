c82a9abe11b7a225b6c3c70b3e89f9bde47a398c
Ondřej Hošek <ondrej.hosek@student.tuwien.ac.at>
2012-11-08 00:09:33 +0100

    detlef.gpodder: Add package-info.java.

--- detlef/src/at/ac/tuwien/detlef/gpodder/package-info.java
+++ detlef/src/at/ac/tuwien/detlef/gpodder/package-info.java
@@ -1,3 +1,7 @@
-/** Responsible for the HTTP and GPodder connections. */
+/**
+ * Provides a {@link PodderService service} for performing background tasks, a {@link GPodderSync
+ * client} for communicating with the service, and a few related interfaces.
+ * @author ondra
+ */
 package at.ac.tuwien.detlef.gpodder;
 
