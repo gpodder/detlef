14127c3bbb861d6176b8c51696a03dc276fd444e
Jakob Gruber <jakob.gruber@gmail.com>
2012-11-09 15:33:18 +0100

    Rudimentary tests for episode list

---
+++ test-detlef/src/at/ac/tuwien/detlef/fragments/EpisodeListFragmentTest.java
@@ -0,0 +1,58 @@
+package at.ac.tuwien.detlef.fragments;
+
+import java.util.UUID;
+
+import android.test.ActivityInstrumentationTestCase2;
+import at.ac.tuwien.detlef.activities.MainActivity;
+import at.ac.tuwien.detlef.db.EpisodeDAOImpl;
+import at.ac.tuwien.detlef.domain.Episode;
+
+import com.jayway.android.robotium.solo.Solo;
+
+public class EpisodeListFragmentTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    private Solo solo;
+    private EpisodeDAOImpl dao;
+    private String uuid;
+    private Episode e;
+
+    public EpisodeListFragmentTest() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    public void setUp() {
+        MainActivity activity = getActivity();
+
+        solo = new Solo(getInstrumentation(), activity);
+        dao = new EpisodeDAOImpl(getActivity());
+
+        uuid = UUID.randomUUID().toString();
+    }
+
+    /**
+     * Upon adding a new episode to the DAO, it should be displayed in the episode list.
+     */
+    public void testAddEpisode() {
+        e = new Episode();
+        e.setTitle(uuid);
+        e.setId(dao.insertEpisode(e));
+        assertTrue(String.format("New episode %s should be displayed in list", uuid), solo.searchText(uuid));
+        assertTrue(String.format("New episode %s should be in DAO", uuid), dao.getAllEpisodes().contains(e));
+    }
+
+    /**
+     * After deleting the newly added episode, it should not be displayed in the episode list,
+     * and should not be contained in the episode DAO.
+     */
+//    public void testRemoveEpisode() {
+//        /* TODO How can I click on a button within a list element? */
+//        assertFalse(String.format("Deleted episode %s should not be displayed in list", uuid), solo.searchText(uuid));
+//        assertFalse(String.format("Deleted episode %s should not be in DAO", uuid), dao.getAllEpisodes().contains(e));
+//    }
+
+    @Override
+    public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+    }
+}
