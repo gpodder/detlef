a95f6a4e3a00477b18c452288cd8f1118b5cd07d
Jakob Gruber <jakob.gruber@gmail.com>
2012-12-01 16:25:22 +0100

    Begin filling the add podcasts result list
    Right now, it consists of two section headers, and dummy popular
    podcasts.

--- detlef/src/at/ac/tuwien/detlef/activities/AddPodcastActivity.java
+++ detlef/src/at/ac/tuwien/detlef/activities/AddPodcastActivity.java
@@ -1,16 +1,30 @@
 
 package at.ac.tuwien.detlef.activities;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.content.Context;
+import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import at.ac.tuwien.detlef.R;
-import at.ac.tuwien.detlef.R.layout;
-import at.ac.tuwien.detlef.R.menu;
+import at.ac.tuwien.detlef.domain.Podcast;
+
+import com.commonsware.cwac.merge.MergeAdapter;
 
 public class AddPodcastActivity extends Activity {
+
+    private final MergeAdapter mergeAdapter = new MergeAdapter();
+    private PodcastListAdapter resultAdapter;
+    private PodcastListAdapter toplistAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +32,40 @@ public class AddPodcastActivity extends Activity {
         setContentView(R.layout.add_podcast_activity);
         // Show the Up button in the action bar.
         getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        /* Set up our merged list. */
+
+        TextView tv = new TextView(this);
+        tv.setText(R.string.search_results);
+        mergeAdapter.addView(tv);
+
+        resultAdapter = new PodcastListAdapter(this, android.R.layout.simple_list_item_1,
+                new ArrayList<Podcast>());
+        mergeAdapter.addAdapter(resultAdapter);
+
+        tv = new TextView(this);
+        tv.setText(R.string.popular_podcasts);
+        mergeAdapter.addView(tv);
+
+        toplistAdapter = new PodcastListAdapter(this, android.R.layout.simple_list_item_1,
+                new ArrayList<Podcast>());
+        fillToplistWithDummies();
+        mergeAdapter.addAdapter(toplistAdapter);
+
+        ListView lv = (ListView) findViewById(R.id.result_list);
+        lv.setAdapter(mergeAdapter);
+    }
+
+    private void fillToplistWithDummies() {
+        Podcast p = new Podcast();
+        p.setTitle("Bestest podcast evar");
+        p.setDescription("This is the bestest bestest bestest bestest bestest bestest bestest bestest podcast evar");
+        toplistAdapter.add(p);
+
+        p = new Podcast();
+        p.setTitle("Somebody set me up the bomb");
+        p.setDescription("This is the bestest bestest bestest bestest bestest bestest bestest bestest podcast evar");
+        toplistAdapter.add(p);
     }
 
     @Override
@@ -37,4 +85,39 @@ public class AddPodcastActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * This adapter displays a list of podcasts which the user can subscribe to.
+     */
+    private static class PodcastListAdapter extends ArrayAdapter<Podcast> {
+
+        private final List<Podcast> podcasts;
+
+        public PodcastListAdapter(Context context, int textViewResourceId, List<Podcast> objects) {
+            super(context, textViewResourceId, objects);
+            podcasts = objects;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View v = convertView;
+            Podcast podcast = podcasts.get(position);
+
+            if (v == null) {
+                LayoutInflater vi = (LayoutInflater) this.getContext()
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                v = vi.inflate(R.layout.add_podcast_list_layout, null);
+            }
+
+            v.setTag(podcast);
+
+            TextView podcastName = (TextView) v.findViewById(R.id.podcast_name);
+            podcastName.setText(podcast.getTitle());
+
+            TextView podcastDesc = (TextView) v.findViewById(R.id.podcast_description);
+            podcastDesc.setText(podcast.getDescription());
+
+            return v;
+
+        }
+    }
 }
