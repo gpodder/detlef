6c649da2691d2ef603f4f3537946fc0ce3cc26db
Martin Lackner <b8d971@b8d971.org>
2012-12-03 09:39:35 +0100

    do not try to insert podcast with no title
    when podcast without title/url arrives do not try to insert it into the
    database

--- detlef/src/at/ac/tuwien/detlef/db/PodcastDBAssistantImpl.java
+++ detlef/src/at/ac/tuwien/detlef/db/PodcastDBAssistantImpl.java
@@ -1,3 +1,4 @@
+
 package at.ac.tuwien.detlef.db;
 
 import java.util.List;
@@ -22,10 +23,14 @@ public class PodcastDBAssistantImpl implements PodcastDBAssistant {
         Log.d(TAG, "applying changes");
         PodcastDAO dao = PodcastDAOImpl.i(context);
         for (Podcast p : changes.getAdd()) {
-            dao.insertPodcast(p);
+            if ((p.getTitle() != null) && (p.getUrl() != null)) {
+                dao.insertPodcast(p);
+            } else {
+                Log.w(TAG, "Cannot insert podcast without title/url");
+            }
         }
 
-        for (Podcast p :changes.getRemove()) {
+        for (Podcast p : changes.getRemove()) {
             Podcast pod = dao.getPodcastByUrl(p.getUrl());
             if (pod != null) {
                 dao.deletePodcast(pod);
